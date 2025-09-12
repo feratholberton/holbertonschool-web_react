62afd39572133ede5b7b2ef8fbddb5c85fc6d78c
Fer <fer.falcon.dg@gmail.com>
2025-09-12 09:30:19 -0300

    0. Creating an interface for a student

---
+++ TypeScript/task_0/js/main.ts
@@ -0,0 +1,22 @@
+interface Student {
+  firstName: string;
+  lastName: string;
+  age: number;
+  location: string;
+}
+
+const student1: Student = {
+  firstName: 'Fer',
+  lastName: 'Falcon',
+  age: 45,
+  location: 'Montevideo',
+}
+
+const student2: Student = {
+  firstName: 'Dan',
+  lastName: 'Raura',
+  age: 45,
+  location: 'Young'
+}
+
+console.log(student2);
