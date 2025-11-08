908fa9aae0eeb61b7474387c2d3f23cfb4ad3bbe
Fer <fer.falcon.dg@gmail.com>
2025-11-08 08:06:52 -0300

    Write Table

--- TypeScript/task_0/js/main.ts
+++ TypeScript/task_0/js/main.ts
@@ -10,13 +10,32 @@ const student1: Student = {
   lastName: 'Falcon',
   age: 45,
   location: 'Montevideo',
-}
+};
 
 const student2: Student = {
   firstName: 'Dan',
   lastName: 'Raura',
-  age: 45,
-  location: 'Young'
-}
+  age: 35,
+  location: 'Young',
+};
 
-console.log(student2);
+const studentsList: Student[] = [student1, student2];
+
+const table: HTMLTableElement = document.createElement('table');
+const tableBody: HTMLTableSectionElement = document.createElement('tbody');
+
+studentsList.forEach((student: Student): void => {
+  const row: HTMLTableRowElement = document.createElement('tr');
+  const firstNameCell: HTMLTableCellElement = document.createElement('td');
+  const locationCell: HTMLTableCellElement = document.createElement('td');
+
+  firstNameCell.textContent = student.firstName;
+  locationCell.textContent = student.location;
+
+  row.appendChild(firstNameCell);
+  row.appendChild(locationCell);
+  tableBody.appendChild(row);
+});
+
+table.appendChild(tableBody);
+document.body.appendChild(table);
