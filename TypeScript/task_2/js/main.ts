1f720e835a0475eec9d51cd0a27162957e389612
Fer <fer.falcon.dg@gmail.com>
2025-11-08 08:54:15 -0300

    7. String literal types

--- TypeScript/task_2/js/main.ts
+++ TypeScript/task_2/js/main.ts
@@ -56,9 +56,21 @@ const executeWork = (employee: Director | Teacher): string => {
   return employee.workTeacherTasks();
 };
 
+type Subjects = 'Math' | 'History';
+
+const teachClass = (todayClass: Subjects): string => {
+  if (todayClass === 'Math') {
+    return 'Teaching Math';
+  }
+  return 'Teaching History';
+};
+
 console.log(createEmployee(200));
 console.log(createEmployee(1000));
 console.log(createEmployee('$500'));
 
 console.log(executeWork(createEmployee(200)));
 console.log(executeWork(createEmployee(1000)));
+
+console.log(teachClass('Math'));
+console.log(teachClass('History'));
